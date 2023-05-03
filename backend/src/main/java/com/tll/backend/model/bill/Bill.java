e03b6573a40666829c9121ea2325b3f7e16645a5
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-05-03 14:26:01 +0700

    add get total price and implement storable object

--- backend/src/main/java/com/tll/backend/model/bill/Bill.java
+++ backend/src/main/java/com/tll/backend/model/bill/Bill.java
@@ -1,11 +1,9 @@
 package com.tll.backend.model.bill;
 
 import com.tll.backend.model.barang.Barang;
-import com.tll.backend.repository.CloneableObject;
 import com.tll.backend.repository.StorableObject;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +11,7 @@ import lombok.experimental.Accessors;
 import org.javatuples.Pair;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
@@ -20,10 +19,15 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
-public class Bill implements Serializable {
+public class Bill implements Serializable, StorableObject<Integer> {
 
     protected Integer id;
     protected Integer userId;
     protected List<Pair<Barang, Integer>> cart;
+
+    public BigDecimal getTotalPrice() {
+        return cart.stream().map(el -> el.getValue0().getHarga().multiply(BigDecimal.valueOf(el.getValue1())))
+                .reduce(new BigDecimal(0), BigDecimal::add, BigDecimal::add);
+    }
 
 }
