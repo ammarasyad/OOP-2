469880b763c638171954afb29890514d2042fb9c
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-05-05 21:03:45 +0700

    move hikari config

--- backend/src/main/java/com/tll/backend/datastore/loader/hikari/HikariConfig.java
+++ backend/src/main/java/com/tll/backend/datastore/loader/hikari/HikariConfig.java
@@ -1,17 +1,19 @@
-package com.tll.backend.datastore.loader.sql;
+package com.tll.backend.datastore.loader.hikari;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 // Enum singleton for thread-safe connection pooling
-public enum SqlConnection implements AutoCloseable {
+public enum HikariConfig implements AutoCloseable {
     INSTANCE;
 
     // For now, the property file is hardcoded
     private static final String PROPERTY_FILE_PATH = "src/main/resources/hikari.properties";
 
     // I think it's better to use Properties, but I'll change it later
-    private static final HikariConfig config = new HikariConfig(PROPERTY_FILE_PATH);
+    private static final com.zaxxer.hikari.HikariConfig config = new com.zaxxer.hikari.HikariConfig(PROPERTY_FILE_PATH);
     private static HikariDataSource dataSource;
 
     static {
@@ -22,6 +24,10 @@ public enum SqlConnection implements AutoCloseable {
 
     public HikariDataSource getDataSource() {
         return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
     @Override
